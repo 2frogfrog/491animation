1ca4ea6cac0dd3803fdfc0bb17f69d87224d0805
2frogfrog <2frogfrog@gmail.com>
2025-01-10 10:57:16 -0800

    Character moves left to right

--- npc.js
+++ npc.js
@@ -2,14 +2,17 @@ class NPC {
     constructor(game) {
         this.game = game;
         this.animator = new Animator(ASSET_MANAGER.getAsset("./npc.png"), 0,0, 32,32,4,0.2);
+        this.speed = 50;
+        this.x = 0;
+        this.y = 0;
     };
 
     update() {
-
+        this.x += this.speed * this.game.clockTick;
     };
 
     draw(ctx) {
-        this.animator.drawFrame(this.game.clockTick,ctx,25,25);
+        this.animator.drawFrame(this.game.clockTick,ctx,this.x,this.y);
     };
         
 }
